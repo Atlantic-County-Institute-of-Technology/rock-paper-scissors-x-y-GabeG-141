8b41d9f3c8344feae59f35cbe051591780218c80
GabeG-141 <gome0121@student.acitech.org>
2025-03-21 09:13:18 -0400

    Still couldn't figure out my js
    Working in boiler plate to get a feel for what I need to do.

--- script.js
+++ script.js
@@ -6,54 +6,19 @@ const nokia = document.getElementById('nokia')
 const WB = document.getElementById('WB')
 const playerScoreBoard = document.getElementById('PS')
 const computerScoreBoard = document.getElementById('CS')
-const playerOptions = ['rock', 'paper', 'scissors', 'gtx970', 'nokia']
-const computerOptions = ['rock', 'paper', 'scissors', 'gtx970', 'nokia']
+const playerChoice = ['rock', 'paper', 'scissors', 'gtx970', 'nokia']
+const computerOpt = ['rock', 'paper', 'scissors', 'gtx970', 'nokia']
 
-const choiceNumber = Math.floor(Math.random());
-const computerChoice = computerOptions[choiceNumber];
+const choiceNumber = Math.floor(Math.random() * 5);
+const computerChoice = computerOpt[choiceNumber];
+
+function unhideF() {
+        const list = document.getElementById('rockD').classList;
+        list.add("unhideF");
+      }
 
 
-
-function winner(player, computer) {
-    let playerScore = 0;
-    let computerScore = 0;
-    if (player === computer) {
-    WB.textContent = "It's a tie."
-    }
-    else if (player == 'rock') {
-        if (computer == 'paper') {
-            WB.textContent = 'Computer Won';
-            computerScore++;
-            computerScoreBoard.textContent = computerScore;
-        } else {
-            WB.textContent = 'Player Won'
-            playerScore++;
-            playerScoreBoard.textContent = playerScore;
-        }
-    }
-    else if (player == 'scissors') {
-        if (computer == 'rock') {
-            WB.textContent = 'Computer Won';
-            computerScore++;
-            computerScoreBoard.textContent = computerScore;
-        } 
-        else {
-            WB.textContent = 'Player Won';
-            playerScore++;
-            playerScoreBoard.textContent = playerScore;
-        }
-    }
-    else if (player == 'paper') {
-        
-        if (computer == 'scissors') {
-            WB.textContent = 'Computer Won';
-            computerScore++;
-            computerScoreBoard.textContent = computerScore;
-        } 
-        else {
-            WB.textContent = 'Player Won';
-            playerScore++;
-            playerScoreBoard.textContent = playerScore;
-        }
-    }
+if (playerOptions === computerOptions) {
+    return
 }
+
