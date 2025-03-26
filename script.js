c8806d81d87434f3753cdb071bbc8a31b364b733
GabeG-141 <gome0121@student.acitech.org>
2025-03-26 08:48:43 -0400

    I did this all in boiler plate. Then I transfered all of it here.

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
-const rock = document.getElementById('rock')
-const paper = document.getElementById('paper')
-const scissors = document.getElementById('scissors')
-const gtx970 = document.getElementById('gtx970')
-const nokia = document.getElementById('nokia')
-const WB = document.getElementById('WB')
-const playerScoreBoard = document.getElementById('PS')
-const computerScoreBoard = document.getElementById('CS')
-const playerChoice = ['rock', 'paper', 'scissors', 'gtx970', 'nokia']
-const computerOpt = ['rock', 'paper', 'scissors', 'gtx970', 'nokia']
-
-const choiceNumber = Math.floor(Math.random() * 5);
-const computerChoice = computerOpt[choiceNumber];
-
-function unhideF() {
-        const list = document.getElementById('rockD').classList;
-        list.add("unhideF");
-      }
+const choices = ['rock', 'paper', 'scissors','gtx970','nokia'];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+const resetButton = document.getElementById("Reset")
+let playerScore = 0;
+let computerScore = 0;
 
 
-if (playerOptions === computerOptions) {
-    return
+function playGame(playerChoice){
+
+    const computerChoice = choices[Math.floor(Math.random() * 5)];
+    let result = "";
+
+    if(playerChoice === computerChoice){
+        result = "IT'S A TIE!"
+    }
+    else{
+        switch(playerChoice){
+            case "rock":
+                result = (computerChoice === "scissors" || computerChoice === "gtx970") ? "YOU WIN!" : "YOU LOSE!";
+        break;
+        case "paper":
+            result = (computerChoice === "rock" || computerChoice === "nokia") ? "YOU WIN!" : "YOU LOSE!";
+            break;
+        case "scissors":
+            result = (computerChoice === "paper" || computerChoice === "nokia") ? "YOU WIN!" : "YOU LOSE!";
+            break;
+            case "gtx970":
+                result = (computerChoice === "paper" || computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "nokia":
+                result = (computerChoice === "gtx970" || computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            }
+    }
+
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `Computer: ${computerChoice}`;
+    resultDisplay.textContent = result;
+
+resultDisplay.classList.remove("greenText", "redText");
+
+    switch(result){
+case "YOU WIN!":
+resultDisplay.classList.add("greenText");
+playerScore++;
+playerScoreDisplay.textContent = playerScore;
+break;
+case "YOU LOSE!":
+resultDisplay.classList.add("redText");
+computerScore++;
+computerScoreDisplay.textContent = computerScore;
+break;
+    }
+function resetGame(){
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreDisplay.textContent = 0;
+    computerScoreDisplay.textContent=0;
 }
-
+resetButton.addEventListener("click", resetGame)
+}
