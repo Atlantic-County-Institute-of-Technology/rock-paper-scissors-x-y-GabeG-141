ee07969263e906acb542e5b0b56ef0bdbabb8980
GabeG-141 <gome0121@student.acitech.org>
2025-04-08 08:20:48 -0400

    Final submit for real this time fr

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-//* To "connect" JS to HTML and CSS
+
+/* The variables from html and css being defined in js */
 const choices = ['rock', 'paper', 'scissors','gtx970','nokia'];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 const playerScoreDisplay = document.getElementById("playerScoreDisplay");
 const computerScoreDisplay = document.getElementById("computerScoreDisplay");
-const resetButton = document.getElementById("Reset")
-const choice_icons = ['👊','🤚','✂️','assets/GTX970.png','assets/nokia.png']
-const playerChoice = document.getElementById('playerChoice');
-const computer_choice = document.getElementById('computerChoice');
+const resetButton = document.getElementById("Reset");
+const choice_icons = ['assets/rock.png','assets/paper.png','assets/scissors.png','assets/GTX970.png','assets/nokia.png'];
 let playerScore = 0;
 let computerScore = 0;
 
 
+/*Function to actually play the game */
 function playGame(playerChoice){
 
     const computerChoice = choices[Math.floor(Math.random() * 5)];
     let result = "";
-
-    //* This decides what wins against what
+    /*choosed what wins against what*/
     if(playerChoice === computerChoice){
         result = "IT'S A TIE!"
     }
     else{
         switch(playerChoice){
             case "rock":
-                result = (computerChoice === "scissors" || computerChoice === "gtx970") ? "YOU WIN!" : "YOU LOSE!";
+                result = (computerChoice === 'scissors' || computerChoice === 'gtx970') ? "YOU WIN!" : "YOU LOSE!";
         break;
         case "paper":
             result = (computerChoice === "rock" || computerChoice === "nokia") ? "YOU WIN!" : "YOU LOSE!";
@@ -39,72 +38,50 @@ function playGame(playerChoice){
             case "nokia":
                 result = (computerChoice === "gtx970" || computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
                 break;
-
             }
     }
 
-function updateUI(user
-    )
 
-    const choiceEmojis = {
-
-        rock: "👊";
-
-        paper: "🤚";
-
-        scissors: "✂️";
-
-        gtx970: "assets/GTX970.png";
-
-        nokia: "assets/nokia.png";
-
-    }
-
-    //* Score tracker
-    playerDisplay.textContent = `${playerChoice}`;
-    computerDisplay.textContent = `${computerChoice}`;
+    
+    
+    playerDisplay.textContent = `PLAYER:`;
+    /* setTimeout to add the "Thinking" delay also the you win / lose text used in the game */
+setTimeout(() => {
+    computerDisplay.textContent = `Computer:`;
     resultDisplay.textContent = result;
 
-resultDisplay.classList.remove("greenText", "redText");
-//* Text that displays if you win / lose and it's color
+    resultDisplay.classList.remove("greenText", "redText");
+
     switch(result){
-case "YOU WIN!":
-resultDisplay.classList.add("greenText");
-playerScore++;
-playerScoreDisplay.textContent = playerScore;
-break;
-case "YOU LOSE!":
-resultDisplay.classList.add("redText");
-computerScore++;
-computerScoreDisplay.textContent = computerScore;
-break;
+            case "YOU WIN!":
+            resultDisplay.classList.add("greenText");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+
+
+        break;
+            case "YOU LOSE!":
+            resultDisplay.classList.add("redText");
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+            break;
     }
-    function updateUI(player, cpu, winner) {
-        result.textContent = winner;
-        
-        // empty out any images stored previously
-        playerChoice.innerHTML = '';
-        computerChoice.innerHTML = '';
-        
-        // create a new image, set the source to the player's
-        // choice, format the size, then append to player_choice
-        let p_img = new Image();
-        p_img.src = choice_icons[player-1];
-        p_img.classList.add('choice_image'); 
-        playerChoice.appendChild(p_img); 
-    
-        // create a new image, set the source to the cpu's
-        // choice, format the size, then append to computer_choice
-        let c_img = new Image();
-        c_img.src = choice_icons[cpu-1];
-        c_img.classList.add('choice_image');
-        computer_choice.appendChild(c_img);
-    
-        player_score.innerText = player_score_val;
-        computer_score.innerText = computer_score_val;
-    }
-    
-    //* Reset button code / function
+
+    /* Sets the images when their respected button is clicked */
+    let c_img = new Image();
+    console.log(choices.indexOf(computerChoice))
+    c_img.src = choice_icons[choices.indexOf(computerChoice)];
+    c_img.classList.add('choice_image');
+    computerDisplay.appendChild(c_img);
+}, 2000);
+    let p_img = new Image();
+    console.log(choices.indexOf(playerChoice))
+    p_img.src = choice_icons[choices.indexOf(playerChoice)];
+    p_img.classList.add('choice_image');
+    playerDisplay.appendChild(p_img);
+
+
+/* reset scores command / code */
 function resetGame(){
     playerScore = 0;
     computerScore = 0;
